3994a163253881381b32cfcceb46093db6c0822a
RuslanSolskyi <rus.solskiy@gmail.com>
2023-05-01 21:27:28 +0300

    add lesson 1

---
+++ lesson1/1.js
@@ -0,0 +1,37 @@
+//1
+let let1 = 'hello'
+let let2 = 'owu'
+let let3 = 'com'
+let let4 = 'ua'
+let let5 = 1
+let let6 = 10
+let let7 = -999
+let let8 = 123
+let let9 = 3.14
+let let10 = 2.7
+let let11 = 16
+let let12 = true
+let let13 = false
+
+console.log (let1)
+console.log (let2)
+console.log (let3)
+console.log (let4)
+console.log (let5)
+console.log (let6)
+console.log (let7)
+console.log (let8)
+console.log (let9)
+console.log (let10)
+console.log (let11)
+console.log (let12)
+console.log (let13)
+//2
+let firstName = 'Сольський'
+let middleName = 'Руслан'
+let lastName = 'Романович'
+let person = (firstName+' '+middleName+' '+lastName)
+console.log (person)
+//3
+let a = 100; let b = '100'; let c = true;
+console.log (typeof a,b,c)
