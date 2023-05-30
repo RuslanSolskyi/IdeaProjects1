62abd7ce6a16d03bdceb90b39cd9e3b810a328a8
RuslanSolskyi <rus.solskiy@gmail.com>
2023-05-30 17:53:24 +0300

    add lesson 9.2

--- lesson9/1.js
+++ lesson9/1.js
@@ -241,3 +241,81 @@ const coursesArray = [
 // Створити для кожного елементу масиву свій блок, блок розділити блоками, в яких будуть зберігатись значення окремих властивостей, для властивості modules зробити список з елементами
 // Приклад структири знаходиться у файлі example.png який лежить в папці з поточним фйлом
 // ------------------
+const coursesArray2 = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular', 'aws', 'docker', 'git', 'java core', 'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular', 'aws', 'docker', 'python core', 'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js', 'python', 'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+
+function createCourseBlocks(courses) {
+    const container3 = document.getElementById('courses-container');
+
+    courses.forEach(course => {
+        const courseBlock = document.createElement('div');
+        courseBlock.classList.add('course-block');
+
+        const titleBlock = document.createElement('div');
+        titleBlock.textContent = course.title;
+        titleBlock.classList.add('title-block');
+        courseBlock.appendChild(titleBlock);
+
+        const infoBlock = document.createElement('div');
+        infoBlock.classList.add('info-block');
+
+        const durationBlock = document.createElement('p');
+        durationBlock.textContent = `Duration: ${course.monthDuration} months, ${course.hourDuration} hours`;
+        durationBlock.classList.add('duration-block');
+        infoBlock.appendChild(durationBlock);
+
+        const modulesBlock = document.createElement('div');
+        modulesBlock.classList.add('modules-block');
+
+        const modulesList = document.createElement('ul');
+        course.modules.forEach(module => {
+            const moduleItem = document.createElement('li');
+            moduleItem.textContent = module;
+            modulesList.appendChild(moduleItem);
+        });
+
+        modulesBlock.appendChild(modulesList);
+        infoBlock.appendChild(modulesBlock);
+
+        courseBlock.appendChild(infoBlock);
+        container3.appendChild(courseBlock);
+    });
+}
+
+createCourseBlocks(coursesArray2);
